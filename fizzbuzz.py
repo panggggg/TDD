d9ed3711feb7d27d25eb9fabfdd2edfdf09956a2
panggggg <p.picczy@gmail.com>
2021-05-31 12:13:07 +0700

    refactor code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,21 +1,33 @@
 # หาร3ลงตัว -> fizz , หาร5ลงตัว -> buzz ถ้าไม่ใช่ return ตัวเลข
-# 3, 6, 9
-# 5, 10, 15
-# 1, 2, 4
+# 3, 6, 9 -> fizz
+# 5, 10, 20 -> buzz
+# 15, 30, 45 -> fizzbuzz
+# 1, 2, 4 -> number
 
 
 def fizzbuzz(num):
 
-    if num % 15 == 0:
-        return "fizzbuzz"
+    result = str(num)
     if num % 3 == 0:
-        return "fizz"
+        result = "fizz"
     if num % 5 == 0:
-        return "buzz"
-    if num % 2 == 0:
-        return "bang"
-    return str(num)
+        result = "buzz"
+    if num % 3 == 0 and num % 5 == 0:
+        result = "fizzbuzz"
+
+    return result
 
 
-for i in range(10):
-    print(i, fizzbuzz(i))
+#     if num % 15 == 0:
+#         return "fizzbuzz"
+#     if num % 3 == 0:
+#         return "fizz"
+#     if num % 5 == 0:
+#         return "buzz"
+#     if num % 2 == 0:
+#         return "bang"
+#     return str(num)
+
+
+# for i in range(10):
+#     print(i, fizzbuzz(i))
