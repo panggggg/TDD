00ee1409adddb5abc1301ef96dda3035a9ec6f09
panggggg <p.picczy@gmail.com>
2021-05-31 12:13:22 +0700

    add test case

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,68 +1,98 @@
+# หาร3ลงตัว -> fizz , หาร5ลงตัว -> buzz ถ้าไม่ใช่ return ตัวเลข
+# 3, 6, 9 -> fizz
+# 5, 10, 20 -> buzz
+# 15, 30, 45 -> fizzbuzz
+# 1, 2, 4 -> number
+
 from fizzbuzz import fizzbuzz
 import unittest
 
 
 class FizzbuzzTest(unittest.TestCase):
-    # def test_fizzbuzz_push_0_should_be_return_0(self):
-    #     result = fizzbuzz(0)
-    #     self.assertEquals("0", result)
-
     def test_fizzbuzz_push_1_should_be_return_1(self):
-        result = fizzbuzz(1)
-        self.assertEquals("1", result)
+        # arrange - prepare before test
+        expected = "1"
+        # action
+        actaul = fizzbuzz(1)
+        # assert
+        self.assertEquals(expected, actaul)
 
-    def test_fizzbuzz_push_2_should_be_return_bang(self):
-        result = fizzbuzz(2)
-        self.assertEquals("bang", result)
+    def test_fizzbuzz_push_2_should_be_return_2(self):
+        # arrange - prepare before test
+        expected = "2"
+        # action
+        actaul = fizzbuzz(2)
+        # assert
+        self.assertEquals(expected, actaul)
+
+    def test_fizzbuzz_push_4_should_be_return_4(self):
+        # arrange - prepare before test
+        expected = "4"
+        # action
+        actaul = fizzbuzz(4)
+        # assert
+        self.assertEquals(expected, actaul)
 
     def test_fizzbuzz_push_3_should_be_return_fizz(self):
-        result = fizzbuzz(3)
-        self.assertEquals("fizz", result)
-
-    def test_fizzbuzz_push_4_should_be_return_bang(self):
-        result = fizzbuzz(4)
-        self.assertEquals("bang", result)
-
-    def test_fizzbuzz_push_5_should_be_return_buzz(self):
-        result = fizzbuzz(5)
-        self.assertEquals("buzz", result)
+        # arrange - prepare before test
+        expected = "fizz"
+        # action
+        actaul = fizzbuzz(3)
+        # assert
+        self.assertEquals(expected, actaul)
 
     def test_fizzbuzz_push_6_should_be_return_fizz(self):
-        result = fizzbuzz(6)
-        self.assertEquals("fizz", result)
-
-    def test_fizzbuzz_push_7_should_be_return_7(self):
-        result = fizzbuzz(7)
-        self.assertEquals("7", result)
-
-    def test_fizzbuzz_push_8_should_be_return_bang(self):
-        result = fizzbuzz(8)
-        self.assertEquals("bang", result)
+        # arrange - prepare before test
+        expected = "fizz"
+        # action
+        actaul = fizzbuzz(6)
+        # assert
+        self.assertEquals(expected, actaul)
 
     def test_fizzbuzz_push_9_should_be_return_fizz(self):
-        result = fizzbuzz(9)
-        self.assertEquals("fizz", result)
+        # arrange - prepare before test
+        expected = "fizz"
+        # action
+        actaul = fizzbuzz(9)
+        # assert
+        self.assertEquals(expected, actaul)
+
+    def test_fizzbuzz_push_5_should_be_return_buzz(self):
+        # arrange - prepare before test
+        expected = "buzz"
+        # action
+        actaul = fizzbuzz(5)
+        # assert
+        self.assertEquals(expected, actaul)
 
     def test_fizzbuzz_push_10_should_be_return_buzz(self):
-        result = fizzbuzz(10)
-        self.assertEquals("buzz", result)
-
-    def test_fizzbuzz_push_11_should_be_return_11(self):
-        result = fizzbuzz(11)
-        self.assertEquals("11", result)
-
-    def test_fizzbuzz_push_12_should_be_return_fizz(self):
-        result = fizzbuzz(12)
-        self.assertEquals("fizz", result)
-
-    def test_fizzbuzz_push_13_should_be_return_13(self):
-        result = fizzbuzz(13)
-        self.assertEquals("13", result)
-
-    def test_fizzbuzz_push_14_should_be_return_bang(self):
-        result = fizzbuzz(14)
-        self.assertEquals("bang", result)
+        # arrange - prepare before test
+        expected = "buzz"
+        # action
+        actaul = fizzbuzz(10)
+        # assert
+        self.assertEquals(expected, actaul)
 
     def test_fizzbuzz_push_15_should_be_return_fizzbuzz(self):
-        result = fizzbuzz(15)
-        self.assertEquals("fizzbuzz", result)
+        # arrange - prepare before test
+        expected = "fizzbuzz"
+        # action
+        actaul = fizzbuzz(15)
+        # assert
+        self.assertEquals(expected, actaul)
+
+    def test_fizzbuzz_push_30_should_be_return_fizzbuzz(self):
+        # arrange - prepare before test
+        expected = "fizzbuzz"
+        # action
+        actaul = fizzbuzz(30)
+        # assert
+        self.assertEquals(expected, actaul)
+
+    def test_fizzbuzz_push_45_should_be_return_fizzbuzz(self):
+        # arrange - prepare before test
+        expected = "fizzbuzz"
+        # action
+        actaul = fizzbuzz(45)
+        # assert
+        self.assertEquals(expected, actaul)
