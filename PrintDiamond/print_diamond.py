03f771308a3a4cdf382822cd6061c10875291557
panggggg <p.picczy@gmail.com>
2021-06-23 10:41:02 +0700

    Refacter at count_space function and add 2 new function for count left space and count middle spcae

--- PrintDiamond/print_diamond.py
+++ PrintDiamond/print_diamond.py
@@ -2,62 +2,22 @@ class Diamond:
     def print_diamond(self, charecter):
         pass
 
-    def print_left_space(self, charecter):
-
-        if charecter == "A":
-            left_space = ""
-
-        if charecter == "B":
-            left_space = ".\n"
-
-        if charecter == "C":
-            left_space = "..\n.\n"
-
-        if charecter == "D":
-            left_space = "...\n..\n.\n"
-
-        return left_space
-
-    def print_right_space(self, charecter):
-        if charecter == "A":
-            right_space = ""
-
-        if charecter == "B":
-            right_space = ".\n"
-
-        if charecter == "C":
-            right_space = "..\n .\n"
-
-        if charecter == "D":
-            right_space = "...\n ..\n .\n"
-        return right_space
-
-    def print_middle_space(self, charecter):
-        if charecter == "A":
-            middle_space = ""
-
-        if charecter == "B":
-            middle_space = [0, 1]
-
-        if charecter == "C":
-            middle_space = [0, 1, 3]
-
-        if charecter == "D":
-            middle_space = [0, 1, 3, 5]
-        return middle_space
-
     def count_space(self, charecter, type, line):
 
+        number_of_charecter = ord(charecter) - 64
         if type == "middle":
-            if line == 1:
-                return 0
-            if charecter == "C" and line == 4:
-                return 1
-            if charecter == "D" and line == 5:
-                return 3
-            if charecter == "D" and line == 6:
-                return 1
-            if charecter == "E" and line == 6:
-                return 5
-            return (line - 2) * 2 + 1
-        return ord(charecter) - 65
+            return self.count_middle(line=line, number_of_charecter=number_of_charecter)
+        else:
+            return self.count_left(number_of_charecter=number_of_charecter)
+
+    def count_left(self, number_of_charecter):
+        return number_of_charecter - 1
+
+    def count_middle(self, line, number_of_charecter):
+        if line == 1:
+            return 0
+        if line - number_of_charecter > 0:
+            number_of_line = number_of_charecter - (line - number_of_charecter)
+        else:
+            number_of_line = line
+        return (number_of_line - 2) * 2 + 1
