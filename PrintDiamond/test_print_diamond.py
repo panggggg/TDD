789a8e9e7a045abbccc09cf34c58202b34a3d372
panggggg <p.picczy@gmail.com>
2021-06-29 09:41:12 +0700

    Add test print diamond push A

--- PrintDiamond/test_print_diamond.py
+++ PrintDiamond/test_print_diamond.py
@@ -91,73 +91,7 @@ class TestPrintDiamond(unittest.TestCase):
         actual = self.diamond.count_space(charecter="E", type="middle", line=8)
         self.assertEquals(expected, actual)
 
-    # def test_left_space_push_charecter_B_should_be_return_list_0_1(self):
-    #     left_space = ".\n"
-    #     expected = left_space
-    #     actual = self.diamond.print_left_space("B")
-    #     self.assertEquals(expected, actual)
-
-    # def test_left_space_push_charecter_C_should_be_return_list_0_1_2(self):
-    #     left_space = "..\n.\n"
-    #     expected = left_space
-    #     actual = self.diamond.print_left_space("C")
-    #     self.assertEquals(expected, actual)
-
-    # def test_left_space_push_charecter_D_should_be_return_list_0_1_2_3(self):
-    #     left_space = "...\n..\n.\n"
-    #     expected = left_space
-    #     actual = self.diamond.print_left_space("D")
-    #     self.assertEquals(expected, actual)
-
-    ############################################################################################
-
-    # def test_right_space_push_charecter_A_should_be_return_list_0_space(self):
-    #     right_space = ""
-    #     expected = right_space
-    #     actual = self.diamond.print_right_space("A")
-    #     self.assertEquals(expected, actual)
-
-    # def test_right_space_push_charecter_B_should_be_return_list_0_1(self):
-    #     right_space = ".\n"
-    #     expected = right_space
-    #     actual = self.diamond.print_right_space("B")
-    #     self.assertEquals(expected, actual)
-
-    # def test_right_space_push_charecter_C_should_be_return_list_0_1_2(self):
-    #     right_space = "..\n .\n"
-    #     expected = right_space
-    #     actual = self.diamond.print_right_space("C")
-    #     self.assertEquals(expected, actual)
-
-    # def test_right_space_push_charecter_D_should_be_return_list_0_1_2_3(self):
-    #     right_space = "...\n ..\n .\n"
-    #     expected = right_space
-    #     actual = self.diamond.print_right_space("D")
-    #     self.assertEquals(expected, actual)
-
-    # ############################################################################################
-
-    # def test_middle_space_push_charecter_A_should_be_return_list_0(self):
-    #     middle_space = ""
-    #     expected = middle_space
-    #     actual = self.diamond.print_middle_space("A")
-    #     self.assertEquals(expected, actual)
-
-    # def test_middle_space_push_charecter_B_should_be_return_list_0_1(self):
-    #     middle_space = "[0, 1]"
-    #     expected = middle_space
-    #     actual = self.diamond.print_middle_space("B")
-    #     self.assertEquals(expected, actual)
-
-    # def test_middle_space_push_charecter_C_should_be_return_list_0_1_3(self):
-    #     middle_space = [0, 1, 3]
-    #     expected = middle_space
-    #     actual = self.diamond.print_middle_space("C")
-    #     self.assertEquals(expected, actual)
-
-    # def test_middle_space_push_charecter_D_should_be_return_list_0_1_3_5(self):
-    #     middle_space = [0, 1, 3, 5]
-    #     # ("  .  \n ...\n.....\n ...\n  .  ")
-    #     expected = middle_space
-    #     actual = self.diamond.print_middle_space("D")
-    #     self.assertEquals(expected, actual)
+    def test_print_diamond_push_A_line_1(self):
+        expected = "0A0"
+        actual = self.diamond.count_space(charecter="A")
+        self.assertEquals(expected, actual)
