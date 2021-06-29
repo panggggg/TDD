2373de81bd3b952a0098daf65caca4e16d8de698
panggggg <p.picczy@gmail.com>
2021-06-29 10:15:31 +0700

    Edit print_diamond

--- PrintDiamond/print_diamond.py
+++ PrintDiamond/print_diamond.py
@@ -2,6 +2,8 @@ class Diamond:
     def print_diamond(self, charecter, line):
         if charecter == "A" and line == 1:
             return "A"
+        if charecter == "B" and line == 1:
+            return "A"
 
     def count_space(self, charecter, type, line):
 
