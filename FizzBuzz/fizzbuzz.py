696c67480034312334e269fd86bd0e850b9cef6e
panggggg <p.picczy@gmail.com>
2021-07-12 09:47:29 +0700

    Add call_fizzbuzz()

--- FizzBuzz/fizzbuzz.py
+++ FizzBuzz/fizzbuzz.py
@@ -1,3 +1,6 @@
+import random
+
+
 def fizzbuzz(num):
 
     if (num / 15) - int(num / 15) == 0:
@@ -6,4 +9,17 @@ def fizzbuzz(num):
         return "fizz"
     if (num / 5) - int(num / 5) == 0:
         return "buzz"
-    return num
+    return str(num)
+
+
+def call_fizzbuzz():
+    rand = int(random.random() * 100)
+    return rand, fizzbuzz(rand)
+
+
+# def random_number():
+#     rand = int(random.random() * 100)
+#     return rand, fizzbuzz(rand)
+
+
+# print(random_number())
