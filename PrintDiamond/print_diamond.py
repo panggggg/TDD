baef55e7a5d12c0483e50e37ec1cb1dec7e2ab3e
panggggg <p.picczy@gmail.com>
2021-07-20 15:11:05 +0700

    Refactor code

--- PrintDiamond/print_diamond.py
+++ PrintDiamond/print_diamond.py
@@ -1,61 +1,50 @@
 class Diamond:
     def print_diamond(self, charecter, line):
 
-        line_of_diamond = (ord(charecter) - 64) - (1 - (ord(charecter) - 64))
+        convert_charecter_to_int = ord(charecter)
 
-        convert_charecter_to_int = ord(charecter) - 1
+        line_of_diamond = self.get_line_of_diamond(
+            convert_charecter_to_int=convert_charecter_to_int
+        )
+
+        number_of_charecter = self.get_number_of_charecter(
+            convert_charecter_to_int=convert_charecter_to_int,
+            line=line,
+        )
 
         ### Alphabet ###
-        if line <= (line_of_diamond + 1) / 2:
-            alphabet = chr(
-                (convert_charecter_to_int * 2 + line) - (64 + (line_of_diamond - 1))
+        if self.is_first_half(line=line, line_of_diamond=line_of_diamond):
+            alphabet = chr(number_of_charecter)
+            number_of_left_right = self.get_number_of_left_right(
+                convert_charecter_to_int=convert_charecter_to_int, line=line
             )
+            space_left_right = "." * int(number_of_left_right)
+
+            number_of_middle = self.get_number_of_middle(line=line)
+            space_middle = "." * int(number_of_middle)
+
         else:
-            alphabet = "A"
+            alphabet = self.get_invert_alphabet(
+                convert_charecter_to_int=convert_charecter_to_int,
+                number_of_charecter=number_of_charecter,
+            )
 
-        number_of_left_right = (ord(charecter) - 64) - line
-        space_left_right = "." * int(number_of_left_right)
+            number_of_left_right = self.get_invert_number_of_left_right(
+                convert_charecter_to_int=convert_charecter_to_int, line=line
+            )
+            space_left_right = "." * int(number_of_left_right)
 
-        number_of_middle = (line - 2) * 2 + 1
-        space_middle = "." * int(number_of_middle)
-
-        ###########################
-        number_of_invert_middle = (line_of_diamond - line) * 2 - 1
-        invert_space_middle = "." * int(number_of_invert_middle)
-
-        invert_number_of_left_right = abs((ord(charecter) - 64) - line)
-        invert_space_left_right = "." * int(invert_number_of_left_right)
+            number_of_middle = self.get_invert_number_of_middle(
+                line=line, line_of_diamond=line_of_diamond
+            )
+            space_middle = "." * int(number_of_middle)
 
         if line == 1 or line == line_of_diamond:
-            number_space_of_first_line = (ord(charecter) - 64) - 1
+            number_space_of_first_line = (convert_charecter_to_int - 64) - 1
             first_line = "." * int(number_space_of_first_line)
             return f"{first_line}{alphabet}{first_line}"
 
-        if charecter == "B":
-            if line == 1:
-                return f"{space_left_right}{alphabet}{space_middle}{alphabet}{space_left_right}"
-            if line == 2:
-                return f"{space_left_right}{alphabet}{space_middle}{alphabet}{space_left_right}"
-
-        if charecter == "C":
-            if line == 1:
-                return "..A.."
-            if line == 2:
-                return ".B.B."
-            if line == 3:
-                return f"{alphabet}{space_middle}{alphabet}"
-            if line == 4:
-                return ".B.B."
-
-        if charecter == "D":
-            if line == 1:
-                return f"{space_left_right}{alphabet}{space_left_right}"
-            # find invert middle
-            if line == 5:
-                return f"{invert_space_left_right}C{invert_space_middle}C{invert_space_left_right}"
-            if line == 6:
-                return f"{invert_space_left_right}B{invert_space_middle}B{invert_space_left_right}"
-            return f"{space_left_right}{alphabet}{space_middle}{alphabet}{space_left_right}"
+        return f"{space_left_right}{alphabet}{space_middle}{alphabet}{space_left_right}"
 
     def count_space(self, charecter, type, line):
 
@@ -80,3 +69,35 @@ class Diamond:
         else:
             number_of_line = line
         return (number_of_line - 2) * 2 + 1
+
+    def is_first_half(self, line, line_of_diamond):
+        return line <= (line_of_diamond + 1) / 2
+
+    def get_line_of_diamond(self, convert_charecter_to_int):
+        return (convert_charecter_to_int - 64) - (1 - (convert_charecter_to_int - 64))
+
+    def get_number_of_charecter(self, convert_charecter_to_int, line):
+        return ((convert_charecter_to_int - 1) * 2 + line) - (
+            64
+            + (
+                self.get_line_of_diamond(
+                    convert_charecter_to_int=convert_charecter_to_int
+                )
+                - 1
+            )
+        )
+
+    def get_number_of_left_right(self, convert_charecter_to_int, line):
+        return (convert_charecter_to_int - 64) - line
+
+    def get_number_of_middle(self, line):
+        return (line - 2) * 2 + 1
+
+    def get_invert_alphabet(self, convert_charecter_to_int, number_of_charecter):
+        return chr(convert_charecter_to_int * 2 - number_of_charecter)
+
+    def get_invert_number_of_left_right(self, convert_charecter_to_int, line):
+        return abs((convert_charecter_to_int - 64) - line)
+
+    def get_invert_number_of_middle(self, line, line_of_diamond):
+        return (line_of_diamond - line) * 2 - 1
