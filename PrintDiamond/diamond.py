b339bce5406f315fabdfc5c3946a2688f3265213
panggggg <p.picczy@gmail.com>
2021-08-09 11:20:37 +0700

    Add a new condition

--- PrintDiamond/diamond.py
+++ PrintDiamond/diamond.py
@@ -1,5 +1,12 @@
 class Diamond:
     def number_of_left_right(self, charector, line):
-        if line == 2:
-            return ord(charector) - 66
-        return ord(charector) - 65
+        if charector == "B":
+            if line == 3:
+                return 1
+        if charector == "C":
+            if line == 4:
+                return 1 
+            if line == 5:
+                return 2
+        if charector == "D":
+            return line - 4    
