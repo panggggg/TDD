22cb433fb47289bca6f19b43084e8f6ef6de5e14
panggggg <p.picczy@gmail.com>
2021-08-09 11:21:28 +0700

    Add test number of left right

--- PrintDiamond/test_diamond.py
+++ PrintDiamond/test_diamond.py
@@ -44,3 +44,74 @@ class TestDiamond(unittest.TestCase):
         diamond = Diamond()
         actual = diamond.number_of_left_right(charector="D", line=2)
         self.assertEquals(expected, actual)
+
+    def test_number_of_left_right_push_C_line_3_should_be_return_0(self):
+        expected = 0
+        diamond = Diamond()
+        actual = diamond.number_of_left_right(charector="C", line=3)
+        self.assertEquals(expected, actual)
+
+    def test_number_of_left_right_push_D_line_3_should_be_return_1(self):
+        expected = 1
+        diamond = Diamond()
+        actual = diamond.number_of_left_right(charector="D", line=3)
+        self.assertEquals(expected, actual)
+
+    def test_number_of_left_right_push_E_line_3_should_be_return_2(self):
+        expected = 2
+        diamond = Diamond()
+        actual = diamond.number_of_left_right(charector="E", line=3)
+        self.assertEquals(expected, actual)
+
+    def test_number_of_left_right_push_D_line_4_should_be_return_0(self):
+        expected = 0
+        diamond = Diamond()
+        actual = diamond.number_of_left_right(charector="D", line=4)
+        self.assertEquals(expected, actual)
+
+    def test_number_of_left_right_push_E_line_4_should_be_return_1(self):
+        expected = 1
+        diamond = Diamond()
+        actual = diamond.number_of_left_right(charector="E", line=4)
+        self.assertEquals(expected, actual)
+
+    def test_number_of_left_right_push_B_line_3_should_be_return_1(self):
+        expected = 1
+        diamond = Diamond()
+        actual = diamond.number_of_left_right(charector="B", line=3)
+        self.assertEquals(expected, actual)
+
+    def test_number_of_left_right_push_C_line_4_should_be_return_1(self):
+        expected = 1
+        diamond = Diamond()
+        actual = diamond.number_of_left_right(charector="C", line=4)
+        self.assertEquals(expected, actual)
+
+    def test_number_of_left_right_push_C_line_5_should_be_return_2(self):
+        expected = 2
+        diamond = Diamond()
+        actual = diamond.number_of_left_right(charector="C", line=5)
+        self.assertEquals(expected, actual)
+
+    def test_number_of_left_right_push_D_line_5_should_be_return_1(self):
+        expected = 1
+        diamond = Diamond()
+        actual = diamond.number_of_left_right(charector="D", line=5)
+        self.assertEquals(expected, actual)
+
+    def test_number_of_left_right_push_D_line_6_should_be_return_2(self):
+        expected = 2
+        diamond = Diamond()
+        actual = diamond.number_of_left_right(charector="D", line=6)
+        self.assertEquals(expected, actual)
+
+    def test_number_of_left_right_push_D_line_7_should_be_return_3(self):
+        expected = 3
+        diamond = Diamond()
+        actual = diamond.number_of_left_right(charector="D", line=7)
+        self.assertEquals(expected, actual)
+    
+
+    
+
+    
