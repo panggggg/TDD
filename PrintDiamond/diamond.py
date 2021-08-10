8984871dc3788094d6fc4a1909ab69f2b0990eac
panggggg <p.picczy@gmail.com>
2021-08-10 14:43:03 +0700

    Add number_of_middle and refactor code

--- PrintDiamond/diamond.py
+++ PrintDiamond/diamond.py
@@ -1,10 +1,30 @@
 class Diamond:
     def number_of_left_right(self, charector, line):
 
-        line_of_diamond =  ord(charector) - 65 + ord(charector) - 64
+        order_of_alphabet = (ord(charector) - 64)
+        line_of_diamond =  order_of_alphabet * 2 - 1
 
-        if line <= (line_of_diamond + 1) / 2:
-            return ord(charector) - (64 + line)
-        return line - (ord(charector) - 64)    
+        if self._is_first_half(line=line, line_of_diamond=line_of_diamond):
+            return order_of_alphabet - line
+        else:
+            return line - order_of_alphabet  
+
+    def number_of_middle(self, charector, line):
+        
+        if line == 2:
+            return 2 - 1
+        if line == 3:
+            return 3 + 0
+        if line == 4:
+            return 4 + 1
+        if charector == "C":
+            return line + 0
+        if charector == "D":
+            return line + 1
+        if charector == "E":
+            return line + 2
+
+    def _is_first_half(self, line, line_of_diamond):
+        return line <= (line_of_diamond + 1) / 2
         
         
