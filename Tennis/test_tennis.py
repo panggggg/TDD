b5b7b70a355cb0eb66fb6d16c54d06c627effb07
panggggg <p.picczy@gmail.com>
2021-09-26 00:03:33 +0700

    Edit test player get score 0-0 and delete test player get score 15-0

--- Tennis/test_tennis.py
+++ Tennis/test_tennis.py
@@ -6,16 +6,8 @@ class TestTennis(unittest.TestCase):
         expected = "0 - 0"
         a = Player("A")
         b = Player("B")
-        a_score = a.get_score("0")
-        b_score = b.get_score("0")
-        self.assertEquals(expected, f"{a_score} - {b_score}")
-
-    def test_player_get_score_15_0(self):
-        expected = "15 - 0"
-        a = Player("A")
-        b = Player("B")
-        a_score = a.get_score("15")
-        b_score = b.get_score("0")
+        a_score = a.get_score()
+        b_score = b.get_score()
         self.assertEquals(expected, f"{a_score} - {b_score}")
 
     def test_add_point_player_get_score_15_0(self):
