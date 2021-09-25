ceeb80fff8495260d8e07d4434321e7a3e17943e
panggggg <p.picczy@gmail.com>
2021-09-26 00:04:34 +0700

    Add class Score, edit at add_point() and get_score()

--- Tennis/tennis.py
+++ Tennis/tennis.py
@@ -1,11 +1,14 @@
+class Score:
+    score = [0, 15, 30, 40, 45]
+
 class Player:
     def __init__(self, name):
         self.name = name
         self.point = 0
     
-    def get_score(self, score):
-        return score
+    def get_score(self):
+        return Score.score[self.point]
 
-    def add_score():
-        pass
+    def add_point(self):
+        self.point += 1
 
