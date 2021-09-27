8803ced7b0df80acd58b65437f4d525b0231865e
panggggg <p.picczy@gmail.com>
2021-09-27 16:05:24 +0700

    Add deuce() in class Scorer and refactor code

--- Tennis/tennis.py
+++ Tennis/tennis.py
@@ -1,38 +1,54 @@
-class Score:
-    score = [0, 15, 30, 40, 45]
-
 class Player:
     def __init__(self, name):
         self.name = name
         self.point = 0
     
-    def get_score(self):
-        return Score.score[self.point]
+    def get_point(self):
+        return self.point
 
     def add_point(self):
         self.point += 1
         return self.point
+    
+    def get_name(self):
+        return self.name
 
 class Scorer:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
     def get_score(self):
-        if self.player1.get_score() == 45:
-            return "Winner is A"
-        if self.player2.get_score() == 45:
-            return "Winner is B"
-        if self.player1.get_score() == 40 and self.player2.get_score() == 40:
+        player1_point = self.player1.get_point()
+        player2_point = self.player2.get_point()
+        if  player1_point == 4 and player2_point < 3:
+            return f"Winner is {self.player1.get_name()}"
+        if player2_point == 4 and  player1_point < 3:
+            return f"Winner is {self.player2.get_name()}"
+        if  player1_point >= 3 and player2_point >= 3:
+            return self.deuce()
+        return f"{self.score(player1_point)} - {self.score(player2_point)}"
+
+    def deuce(self):
+        player1Name = self.player1.get_name()
+        player2Name = self.player2.get_name()
+        score = self.player1.get_point() - self.player2.get_point()
+        if score == 0:
             return "Deuce"
-        else:
-            return f"{self.get_point(self.player1.get_score())} - {self.get_point(self.player2.get_score())}"
-    
-    def get_point(self, point):
+        if score == 1:
+            return f"Advantage {player1Name}"
+        if score == 2:
+            return f"Winner is {player1Name}"
+        if score == -1:
+            return f"Advantage {player2Name}"
+        if score == -2:
+            return f"Winner is {player2Name}"
+
+    def score(self, point):
         if point == 0:
             return "Lover"
-        if point == 15:
+        if point == 1:
             return "Fifthteen"
-        if point == 30:
+        if point == 2:
             return "Thirty"
-        if point == 40:
+        if point == 3:
             return "Fourty"
